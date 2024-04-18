037a0cf1a99b1fe065fd021c2d0164c8e401288d
Jesus Carrasco <Jesuscarrascoyel.mur@gmail.com>
2024-04-18 11:45:52 +0200

    Mod Home Desing

--- src/assets/common/Header.jsx
+++ src/assets/common/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import logo from '../../../public/img/FileCoMMite-log.png'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export const Header = () => {
     return (
-        <div className="row">
-            <div className="col-md-3"></div>
-            <div className="col-md-6"></div>
-            <div className="col-md-6"></div>
-        </div>
+        <>
+            <div className="row header">
+                <div className="col-md-3"><img src={logo} /></div>
+                <div className="col-md-6"><h1>SHARE FILES</h1></div>
+                <div className="col-md-3"><h3>INFO</h3></div>
+            </div>
+        </>
     )
 }
--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -2,10 +2,31 @@ import React from 'react'
 import { Header } from '../common/Header'
 
 export const Home = () => {
+
+    const listMeetings = () => {
+        return (
+            <div className="row meeting-row">
+                <div className="col-md-8 meeting-specs">{/*Nombre Reunion */}
+                    <strong>FileCommite</strong>
+                </div>
+                <div className="col-md-4 meeting-specs">{/*Botones Conectar / Desconectar */}
+                    <button>Go</button>
+                    <button>Delete</button>
+
+                </div>
+            </div>
+        )
+
+    }
     return (
         <>
             <Header />
+            <div className="container meetings-section">
+                <div className="col-md-6 meetings-container">
+                    {listMeetings()}
 
+                </div>
+            </div>
         </>
     )
 }
