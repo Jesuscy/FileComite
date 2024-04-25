ead68fe997b3c808ab6f7632f983fc406204728a
Jesus Carrasco <Jesuscarrascoyel.mur@gmail.com>
2024-04-25 20:23:08 +0200

    Mod add Desing

---
+++ src/assets/common/NewMeeting.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export const NewMeeting = () => {
+    return (
+
+        <div className="container create-meeting-form">
+            <div className="row">
+
+                <div className="col-md-6 create-meeting-prop">
+                    <div className='create-meeting-input'>
+                        <label for="name">Server Name:</label>
+                        <input name="name"></input>
+                    </div>
+                </div>
+                <div className="col-md-6 create-meeting-prop">
+                    <div className='create-meeting-input'>
+                        <label for="name">Server :</label>
+                        <input name="name"></input>
+                    </div></div>
+
+                <div className="col-md-6 create-meeting-prop">
+                    <div className='create-meeting-input'>
+                        <label for="name">Server Name:</label>
+                        <input name="name"></input>
+                    </div></div>
+                <div className="col-md-6 create-meeting-prop">
+                    <div className='create-meeting-input'>
+                        <label for="name">Server Name:</label>
+                        <input name="name"></input>
+                    </div></div>
+            </div>
+
+
+        </div>
+    )
+}
--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Header } from '../common/Header'
 import { Link } from 'react-router-dom'
+import { NewMeeting } from '../common/NewMeeting'
 
 export const Home = () => {
 
@@ -36,17 +37,18 @@ export const Home = () => {
         <>
             <Header />
             <div className="row meetings-section">
-                <div className="col-md-2 meetings-info">
-                    <div className="row recent-meetings"><p>Recent Meetings</p>
-                        <div className="col-md-12 comp-list">
+                <div className="col-md-2 col-sm-12 col-xs-12 meetings-info">
+                    <div className="row recent-meetings">
+                        <div className="col-md-12">
+                            <p>Recent Meetings</p>
                             {listMeetings()}
-
 
                         </div>
 
-                        <p>Recent Messages</p>
 
-                        <div className="col-md-12 comp-list">
+                        <div className="col-md-12">
+                            <p>Recent Messages</p>
+
                             {listMeetings()}
                             {listMeetings()}
                             {listMeetings()}
@@ -54,8 +56,37 @@ export const Home = () => {
                     </div>
 
                 </div>
-                <div className="col-md-10 meetings-creator">
-                    HOLAS
+                <div className="col-md-10 col-sm-12 col-xs-12">
+
+                    <div className="container">
+                        <div className="meetings-creator">
+                            <div className="col-md-3 meetings-options">
+                                <div className="row">
+                                    <div className="col-md-12">
+                                        <div className="meeting-option">
+                                            Create a new Meeting
+                                        </div>
+                                    </div>
+                                    <div className="col-md-12">
+                                        <div className="meeting-option">
+                                            Shearch Meeting
+                                        </div>
+                                    </div>
+                                    <div className="col-md-12">
+                                        <div className="meeting-option">
+                                            Info and tutorials.
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                            <div className="col-md-9">{<NewMeeting />}</div>
+
+
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
